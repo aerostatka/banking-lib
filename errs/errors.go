da938e87f1b6c6126d918e5f6f089fd3244d2631
Daria Frolova <frolova@virtualhealth.com>
2021-08-22 21:16:00 -0400

    Banking training lib module

---
+++ errs/errors.go
@@ -0,0 +1,49 @@
+package errs
+
+import "net/http"
+
+type AppError struct {
+	Code    int    `json:",omitempty"`
+	Message string `json:"message"`
+}
+
+func (err AppError) AsMessage() *AppError {
+	return &AppError{
+		Message: err.Message,
+	}
+}
+
+func NewNotFoundError(message string) *AppError {
+	return &AppError{
+		Code:    http.StatusNotFound,
+		Message: message,
+	}
+}
+
+func NewInternalServerError(message string) *AppError {
+	return &AppError{
+		Code:    http.StatusInternalServerError,
+		Message: message,
+	}
+}
+
+func NewValidationError(message string) *AppError {
+	return &AppError{
+		Code:    http.StatusUnprocessableEntity,
+		Message: message,
+	}
+}
+
+func NewUnauthorizedError(message string) *AppError {
+	return &AppError{
+		Code:    http.StatusUnauthorized,
+		Message: message,
+	}
+}
+
+func NewForbiddenError(message string) *AppError {
+	return &AppError{
+		Code:    http.StatusForbidden,
+		Message: message,
+	}
+}
---
+++ go.mod
@@ -0,0 +1,5 @@
+module githib.com/aerostatka/banking-lib
+
+go 1.16
+
+require go.uber.org/zap v1.19.0 // indirect
